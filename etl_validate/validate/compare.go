f13453888c3bfcef18f444a186cac5109f313318
chensong <chensong@jztey.com>
2021-07-13 17:08:20 +0800

    添加新表比对规则

--- etl_validate/validate/compare.go
+++ etl_validate/validate/compare.go
@@ -5,18 +5,19 @@ import (
 	"study/etl_validate/common"
 )
 
-func main()(){
+func main() {
 	var dir string
-	flag.StringVar(&dir,"d","目录名","目录名")
+	flag.StringVar(&dir, "d", "目录名", "目录名")
 	flag.Parse()
 	//对比的表名
-	var tables []string = []string{"tb_gos_stock_stockpreemption"}
+	var tables []string = []string{"tb_gos_stock_stockpreemption", "vw_common_prod"}
 	//对比的表列名
 	var fields [][]string = [][]string{
-		[]string{"pk", "fk", "createtime", "lastmodifytime","version", "lineid", "branchid", "deleteflag", "note", "preemptionpreemption", "prodid", "lotno", "quantity", "rowguid", "billid", "whseid", "storeid", "billguid", "opid", "custid", "custno", "custname"}}
+		[]string{"pk", "fk", "createtime", "lastmodifytime", "version", "lineid", "branchid", "deleteflag", "note", "preemptionpreemption", "prodid", "lotno", "quantity", "rowguid", "billid", "whseid", "storeid", "billguid", "opid", "custid", "custno", "custname"},
+		[]string{"pk", "branchid", "prodid", "prodno", "deleteflag", "version"}}
 	//需要对比的表列名 对比的列名才会打印出来
 	var compareFields [][]string = [][]string{
-		[]string{"version", "branchid", "storeid", "prodid", "quantity","lastmodifytime","deleteflag"}}
-	common.CommonCompare(&dir,&tables,&fields,&compareFields)
+		[]string{"version", "branchid", "storeid", "prodid", "quantity", "lastmodifytime", "deleteflag"},
+		[]string{"branchid", "prodid", "prodno", "deleteflag", "version"}}
+	common.CommonCompare(&dir, &tables, &fields, &compareFields)
 }
-
