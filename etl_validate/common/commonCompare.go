a4926fbcfee85e6dc45f96d5d232a5ab8eef3bdd
chensong <chensong@jztey.com>
2021-07-13 16:05:25 +0800

    修复-.开头的负数小数判断

--- etl_validate/common/commonCompare.go
+++ etl_validate/common/commonCompare.go
@@ -116,6 +116,10 @@ func zeroNumberHandle(a string) (r string) {
 		//不是个数字 原样返回
 		return a
 	}
+	hasDotMinus := strings.Index(a,"-.")==0
+	if hasDotMinus{
+		a = strings.Replace(a,"-.","-0.",-1)
+	};
 	r = a
 	for {
 		if strings.LastIndex(r, "0") == len(r)-1 {
